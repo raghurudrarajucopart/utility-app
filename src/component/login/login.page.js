b424c22678d6a2cd3f5b715f9e2f40a7d7553afa
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2018-06-12 12:36:26 +0530

    Added login form using material-ui

--- src/component/login/login.page.js
+++ src/component/login/login.page.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 
 class LoginPage extends Component {
   constructor() {
@@ -6,7 +9,23 @@ class LoginPage extends Component {
   }
   render() {
     return(
-      <div>LoginPage</div>
+      <div className="login-page-container">
+        <h2>LoginPage</h2>
+          <div className="login-form-container">
+            <form name="loginForm" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <TextField required id="username" label="User Name" margin="normal" />
+              </div>
+              <div className="form-group">
+                <TextField required id="password" label="Password" type="password" margin="normal" />
+              </div>
+              <div className="form-group">
+                <Button variant="contained" color="primary" > Submit
+                </Button>
+              </div>
+            </form>
+          </div>
+      </div>
     )
   }
 }
